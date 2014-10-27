833bcf273d8901ba435cde3ce1b7314243151003
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2014-10-27 16:09:35 -0400

    Simple comment searching

---
+++ run.rb
@@ -0,0 +1,31 @@
+
+# I'm starting with a hacky script to
+# experiment with how to do this.
+#
+# This project probably won't end up
+# being written in Ruby. Who knows though.
+#
+# THIS IS SHITTY CODE AND I KNOW IT!
+#
+# Let the scripting BEGIN!
+
+require 'sass'
+
+puts Sass::VERSION
+
+
+tree = Sass::Engine.new(File.open("input.scss").read, :syntax => :scss).to_tree
+
+def search(node)
+  if node.is_a? Sass::Tree::CommentNode
+    show_comment(node)
+  else
+    node.children.each { |n| search(n) }
+  end
+end
+
+def show_comment(node)
+  puts node.resolved_value
+end
+
+search(tree)
